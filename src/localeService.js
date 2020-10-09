cd1d17f9a6b0520feb3dc8bc4270b3adc1c86e3b
Lewis Prescott <Lewis.Prescott@cancer.org.uk>
2020-10-09 08:59:38 +0100

    locale service task

--- src/localeService.js
+++ src/localeService.js
@@ -1,4 +1,5 @@
 
+// TODO: replace mock response with API
 function getLocalGreeting(language) {
     switch(language) {
       case "en":
@@ -10,6 +11,25 @@ function getLocalGreeting(language) {
       default:
         return "👋";
     }
+}
+
+/**
+* Example Response, List of locale messages:
+* [ {"locale": "en", "message": "Hello!"} ]
+*/
+async function getAllLocales() {
+  try {
+    const response = await axios.get(`http://localhost/v1/locales`,
+      { headers: { Authorization: "Bearer Token" } }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error.message);
+    return {
+        error: error.error,
+        message: error.message
+    }
+  }
 }
 
 module.exports = {
